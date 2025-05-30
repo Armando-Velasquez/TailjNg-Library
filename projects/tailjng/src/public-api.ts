3d61f3a2d17e361beb5f20f7ee59e6ef5004f104
JosueAVD27 <ajvelasquez4@espe.edu.ec>
2025-05-30 00:16:16 -0500

    refactor: remove unused services and components
    - Deleted MenuService and SidebarService as they were no longer needed.
    - Removed InstallingComponent and its associated HTML template.
    - Cleaned up HomeComponent and TailwindComponent by removing their implementations.
    - Eliminated ThemingComponent and its template, streamlining the application.
    - Removed index.html and main.ts to simplify the project structure.
    - Deleted styles.scss and related TypeScript configuration files.
    - Updated ng-package.json to include style paths for better organization.
    - Commented out exports in public-api.ts to prevent unnecessary exports.
    - Cleaned up tailjng-theme.css by removing unused utility classes.
    - Adjusted tsconfig.json to set baseUrl and module for better module resolution.

--- projects/tailjng/src/public-api.ts
+++ projects/tailjng/src/public-api.ts
@@ -14,65 +14,65 @@
 // export * from './lib/tailjng.service';
 // export * from './lib/tailjng.component';
 
-// Mode Toggle
-export * from './lib/components/mode-toggle/mode-toggle.component';
+// // Mode Toggle
+// export * from './lib/components/mode-toggle/mode-toggle.component';
 
-// Tooltip
-export * from './lib/components/tooltip/tooltip.directive';
+// // Tooltip
+// export * from './lib/components/tooltip/tooltip.directive';
 
-// Label
-export * from './lib/components/label/label.component';
+// // Label
+// export * from './lib/components/label/label.component';
 
-// Input
-export * from './lib/components/input/input.component';
+// // Input
+// export * from './lib/components/input/input.component';
 
-// Checkbox
-export * from './lib/components/checkbox/checkbox.component';
+// // Checkbox
+// export * from './lib/components/checkbox/checkbox.component';
 
-// Button
-export * from './lib/components/button/button.component';
+// // Button
+// export * from './lib/components/button/button.component';
 
-// Dialog
-export * from './lib/shared/dialog.shared';
-export * from './lib/components/dialog/dialog.component';
+// // Dialog
+// export * from './lib/shared/dialog.shared';
+// export * from './lib/components/dialog/dialog.component';
 
-// Alert Toast
-export * from './lib/components/alert-toast/elements/alert-toast.service';
-export * from './lib/components/alert-toast/alert-toast.component';
+// // Alert Toast
+// export * from './lib/components/alert-toast/elements/alert-toast.service';
+// export * from './lib/components/alert-toast/alert-toast.component';
 
-// Alert Dialog
-export * from './lib/components/alert-dialog/elements/alert-dialog.service';
-export * from './lib/components/alert-dialog/alert-dialog.component';
+// // Alert Dialog
+// export * from './lib/components/alert-dialog/elements/alert-dialog.service';
+// export * from './lib/components/alert-dialog/alert-dialog.component';
 
-// Http
-export * from './lib/http/api-url';
-export * from './lib/http/converter.service';
-export * from './lib/http/crud-generic.service';
-export * from './lib/http/http-rest.service';
-export * from './lib/http/http-error.service';
+// // Http
+// export * from './lib/http/api-url';
+// export * from './lib/http/converter.service';
+// export * from './lib/http/crud-generic.service';
+// export * from './lib/http/http-rest.service';
+// export * from './lib/http/http-error.service';
 
-// Select
-export * from './lib/components/select/select.component';
+// // Select
+// export * from './lib/components/select/select.component';
 
-// Toggle Radio
-export * from './lib/components/toggle-radio/toggle-radio.component';
+// // Toggle Radio
+// export * from './lib/components/toggle-radio/toggle-radio.component';
 
-// Theme
-export * from './lib/colors/theme/theme.component';
+// // Theme
+// export * from './lib/colors/theme/theme.component';
 
-// Crud
-export * from './lib/components/crud/card-component/card.component';
+// // Crud
+// export * from './lib/components/crud/card-component/card.component';
 
-export * from './lib/components/crud/filter-component/filter.component';
-export * from './lib/components/crud/filter-component/elements/filter.interface';
+// export * from './lib/components/crud/filter-component/filter.component';
+// export * from './lib/components/crud/filter-component/elements/filter.interface';
 
-export * from './lib/shared/form.shared';
-export * from './lib/components/crud/form-component/form.component';
-export * from './lib/components/crud/form-component/components/content-form/content-form.component';
-export * from './lib/components/crud/form-component/components/error-message/error-message.component';
+// export * from './lib/shared/form.shared';
+// export * from './lib/components/crud/form-component/form.component';
+// export * from './lib/components/crud/form-component/components/content-form/content-form.component';
+// export * from './lib/components/crud/form-component/components/error-message/error-message.component';
 
-export * from './lib/components/crud/paginator-component/paginator.component';
+// export * from './lib/components/crud/paginator-component/paginator.component';
 
-export * from './lib/components/crud/table-component/table.component';
-export * from './lib/components/crud/table-component/elements/table.interface';
+// export * from './lib/components/crud/table-component/table.component';
+// export * from './lib/components/crud/table-component/elements/table.interface';
 
